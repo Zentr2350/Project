6cb388cd14c07299dc07e6ce82813868268aaa5a
Zentr2350 <93312666+Zentr2350@users.noreply.github.com>
2021-10-29 17:42:28 +0300

    Update graphic.py

--- graphic.py
+++ graphic.py
@@ -3,29 +3,32 @@ from PIL import Image, ImageDraw
 
 im = Image.new("RGB", (1000, 1000), (255, 255, 255))
 draw = ImageDraw.Draw(im)
-draw.line((500, 0, 500, 1000), fill=(0, 0, 0), width=5)
-draw.line((0, 500, 1000, 500), fill=(0, 0, 0), width=5)
+draw.line((500, 0, 500, 1000), fill=(0, 0, 0), width=3)
+draw.line((0, 500, 1000, 500), fill=(0, 0, 0), width=3)
 for i in range(0, 1001, 50):
-    draw.line((i, 505, i, 495), fill=(0, 0, 0), width=5)
-    draw.line((495, i, 505, i), fill=(0, 0, 0), width=5)
-c = ''
+    draw.line((i, 505, i, 495), fill=(0, 0, 0), width=3)
+    draw.line((495, i, 505, i), fill=(0, 0, 0), width=3)
 a = input()
-for i in range(-10000, 10000):
-    a = a.replace('x', 'i')
+cht = ''
+for irt in range(-10000, 10000):
+    a = a.replace('x', 'irt')
 
     try:
-        a = a.replace('c', 'i')
+        a = a.replace('cht', 'irt')
 
-
-        i /= 1000
-        c = i - (1/1000)
-        draw.line((i * 50 + 500, (0 - eval(a)) * 50 + 500, i * 50 + 500, (0 - eval(a)) * 50 + 500), fill=(0, 0, 0), width=3)
-        a = a.replace('i', 'c')
-        draw.line((i * 50 + 500, (0 - eval(a.replace('c', 'i'))) * 50 + 500, c * 50 + 500, (0 - eval(a)) * 50 + 500), fill=(0, 0, 0),
-                  width=3)
+        irt /= 1000
+        cht = irt - (1 / 1000)
+        draw.line((irt * 50 + 500, (0 - eval(a)) * 50 + 500, irt * 50 + 500, (0 - eval(a)) * 50 + 500), fill=(0, 0, 0),
+                  width=2)
+        a = a.replace('irt', 'cht')
+        if abs(((0 - eval(a.replace('cht', 'irt'))) * 50 + 500) - ((0 - eval(a)) * 50 + 500)) < 100:
+            draw.line(
+                (irt * 50 + 500, (0 - eval(a.replace('cht', 'irt'))) * 50 + 500, cht * 50 + 500, (0 - eval(a)) * 50 + 500),
+                fill=(0, 0, 0),
+                width=2)
 
     except Exception as e:
-        # print(e)
+        print(e)
         pass
-im.save('gr.jpg')
 
+im.save('gr.jpg')
