1ade18f702bc0544864fe0aa6f23d795a7a9a98c
Zentr2350 <93312666+Zentr2350@users.noreply.github.com>
2021-10-30 15:51:46 +0300

    Update graphic.py

--- graphic.py
+++ graphic.py
@@ -9,6 +9,7 @@ for i in range(0, 1001, 50):
     draw.line((i, 505, i, 495), fill=(0, 0, 0), width=3)
     draw.line((495, i, 505, i), fill=(0, 0, 0), width=3)
 a = input()
+l = int(input())
 cht = ''
 for irt in range(-10000, 10000):
     a = a.replace('x', 'irt')
@@ -18,12 +19,12 @@ for irt in range(-10000, 10000):
 
         irt /= 1000
         cht = irt - (1 / 1000)
-        draw.line((irt * 50 + 500, (0 - eval(a)) * 50 + 500, irt * 50 + 500, (0 - eval(a)) * 50 + 500), fill=(0, 0, 0),
+        draw.line((irt * 50 * l + 500, (0 - eval(a)) * l * 50 + 500, irt * l * 50 + 500, (0 - eval(a)) * l * 50 + 500), fill=(0, 0, 0),
                   width=2)
         a = a.replace('irt', 'cht')
-        if abs(((0 - eval(a.replace('cht', 'irt'))) * 50 + 500) - ((0 - eval(a)) * 50 + 500)) < 100:
+        if abs(((0 - eval(a.replace('cht', 'irt'))) * 50 * l + 500) - ((0 - eval(a)) * l * 50 + 500)) < 100:
             draw.line(
-                (irt * 50 + 500, (0 - eval(a.replace('cht', 'irt'))) * 50 + 500, cht * 50 + 500, (0 - eval(a)) * 50 + 500),
+                (irt * 50 * l + 500, (0 - eval(a.replace('cht', 'irt'))) * l * 50 + 500, cht * 50 * l + 500, (0 - eval(a)) * l * 50 + 500),
                 fill=(0, 0, 0),
                 width=2)
 
