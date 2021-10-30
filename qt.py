9a17aa3385ec5a499bc6c0ee8e45443132f4e28a
Zentr2350 <93312666+Zentr2350@users.noreply.github.com>
2021-10-30 15:51:51 +0300

    Update qt.py

--- qt.py
+++ qt.py
@@ -3,10 +3,10 @@ from math import *
 from PIL import Image, ImageDraw
 
 from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QTabWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox
 from PyQt5.QtGui import QPixmap
 
-def gr(a):
+def gr(a, o):
     im = Image.new("RGB", (1000, 1000), (255, 255, 255))
     draw = ImageDraw.Draw(im)
     draw.line((500, 0, 500, 1000), fill=(0, 0, 0), width=3)
@@ -15,7 +15,7 @@ def gr(a):
         draw.line((i, 505, i, 495), fill=(0, 0, 0), width=3)
         draw.line((495, i, 505, i), fill=(0, 0, 0), width=3)
     cht = ''
-    for irt in range(-10000, 10000):
+    for irt in range(int(-10000 // o), int(10000 // o)):
         a = a.replace('x', 'irt')
 
         try:
@@ -23,14 +23,14 @@ def gr(a):
 
             irt /= 1000
             cht = irt - (1 / 1000)
-            draw.line((irt * 50 + 500, (0 - eval(a)) * 50 + 500, irt * 50 + 500, (0 - eval(a)) * 50 + 500),
+            draw.line((irt * 50 * o + 500, (0 - eval(a)) * o * 50 + 500, irt * o * 50 + 500, (0 - eval(a)) * o * 50 + 500),
                       fill=(0, 0, 0),
                       width=2)
             a = a.replace('irt', 'cht')
-            if abs(((0 - eval(a.replace('cht', 'irt'))) * 50 + 500) - ((0 - eval(a)) * 50 + 500)) < 100:
+            if abs(((0 - eval(a.replace('cht', 'irt'))) * 50 * o + 500) - ((0 - eval(a)) * o * 50 + 500)) < 100:
                 draw.line(
-                    (irt * 50 + 500, (0 - eval(a.replace('cht', 'irt'))) * 50 + 500, cht * 50 + 500,
-                     (0 - eval(a)) * 50 + 500),
+                    (irt * 50 * o + 500, (0 - eval(a.replace('cht', 'irt'))) * o * 50 + 500, cht * o * 50 + 500,
+                     (0 - eval(a)) * 50 * o + 500),
                     fill=(0, 0, 0),
                     width=2)
 
@@ -50,11 +50,27 @@ class MyWidget(QMainWindow):
         self.pixmap = QPixmap('shablon.jpg')
         self.image = self.label_7
         self.image.setPixmap(self.pixmap)
+        self.pushButton_2.clicked.connect(self.x)
+        self.k = 1
+
+
+    def x(self):
+        if self.comboBox.currentText() == 'Увеличить':
+            self.k *= int(self.lineEdit_2.text())
+        else:
+            self.k /= int(self.lineEdit_2.text())
+        gr(self.lineEdit.text(), self.k)
+
+        self.pixmap = QPixmap('gr.jpg')
+        self.image = self.label
+        self.image.setPixmap(self.pixmap)
+
+
 
 
 
     def gra(self):
-        gr(self.lineEdit.text())
+        gr(self.lineEdit.text(), 1)
         self.pixmap = QPixmap('gr.jpg')
         self.image = self.label
         self.image.setPixmap(self.pixmap)
